64caa02d0088e19cb46e7b3e4ec196e3a10eca78
Leon Berenschot <leonb@beriedata.nl>
2010-03-04 12:18:28 +0100

    changed nifty_scaffold controller layout

--- rails_generators/nifty_scaffold/templates/controller.rb
+++ rails_generators/nifty_scaffold/templates/controller.rb
@@ -1,3 +1,13 @@
 class <%= plural_class_name %>Controller < ApplicationController
+  # before_filter :login_required
+  before_filter :find_<%= singular_name %>, :except => [:index, :new, :create]
+
   <%= controller_methods :actions %>
+
+protected
+
+  def find_<%= singular_name %>
+    @<%= singular_name %> = <%= class_name %>.find(params[:id])
+  end
+
 end
--- rails_generators/nifty_scaffold/templates/migration.rb
+++ rails_generators/nifty_scaffold/templates/migration.rb
@@ -1,4 +1,5 @@
 class Create<%= plural_class_name %> < ActiveRecord::Migration
+
   def self.up
     create_table :<%= plural_name %> do |t|
     <%- for attribute in attributes -%>
@@ -9,8 +10,9 @@ class Create<%= plural_class_name %> < ActiveRecord::Migration
     <%- end -%>
     end
   end
-  
+
   def self.down
     drop_table :<%= plural_name %>
   end
+
 end
