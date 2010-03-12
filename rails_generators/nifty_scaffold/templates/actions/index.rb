dee5bd579d0741829656edc3a3184467acd9295a
Leon Berenschot <leonb@beriedata.nl>
2010-03-12 01:42:56 +0100

    Pagination (required will_paginate)

--- rails_generators/nifty_scaffold/templates/actions/index.rb
+++ rails_generators/nifty_scaffold/templates/actions/index.rb
@@ -1,5 +1,5 @@
   def index
-    @<%= plural_name %> = <%= class_name %>.all
+    @<%= plural_name %> = <%= class_name %>.paginate :page => params[:page] #, :order => order, :conditions => conditions
     respond_to do |format|
       format.html
       format.js { render :layout => false }
